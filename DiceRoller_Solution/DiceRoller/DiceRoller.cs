5a2e949143a39cfb4d4f1ee3941882034267a4b3
eerotpt <eero.sakson@tptlive.ee>
2017-11-19 19:45:00 +0200

    sec_sun

---
+++ DiceRoller_Solution/DiceRoller/DiceRoller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller
+{
+    using System.Collections.Generic;
+
+    public class DiceRoller
+    {
+        public List<DiceRoll> Roll(List<Dice> dices)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- DiceRoller_Solution/DiceRoller/Program.cs
+++ DiceRoller_Solution/DiceRoller/Program.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace DiceRoller
 {
-    class Program
+    using System;
+    using System.Collections.Generic;
+
+    public class Program
     {
         static void Main(string[] args)
         {
@@ -17,24 +20,24 @@ namespace DiceRoller
             Dice d6 = Dice.D6;
 
             var total = 0;
+
             for (var i = 0; i < 3; i++)
             {
                 var roll = d6.Roll();
-                Console.WriteLine($"1{ d6.Description }: { roll }");
                 total += roll;
+                Console.WriteLine($"1{ d6.Description }: { roll }");
             }
 
             Dice d8 = Dice.D8;
             for (var i = 0; i < 2; i++)
             {
                 var roll = d8.Roll();
-                Console.WriteLine($"1{ d8.Description }: { roll }");
                 total += roll;
+                Console.WriteLine($"1{ d8.Description }: { roll }");
             }
+
             Console.WriteLine();
-            Console.WriteLine($"Roll total: {total}");
-            Console.WriteLine();
-            Console.Write("> ");
+            Console.Write(">");
             Console.ReadLine();
 
         }
