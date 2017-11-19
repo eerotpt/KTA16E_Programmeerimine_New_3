272a744fd83032ec4009c3a8bb029309daf58a8c
eerotpt <eero.sakson@tptlive.ee>
2017-11-19 19:08:27 +0200

    Initial commit

---
+++ DiceRoller_Solution/DiceRoller/Dice.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller
+{
+    public class Dice
+    {
+        public int Sides { get; }
+        public string Description { get; }
+        private Random Random { get; } = new Random();
+
+        private Dice(int sides, string description)
+        {
+            this.Sides = sides;
+            this.Description = description;
+        }
+
+        public static Dice D6 => new Dice(6, "d6");
+        public static Dice D8 => new Dice(6, "d8");
+
+        public int Roll()
+        {
+            return this.Random.Next(1, this.Sides + 1);
+        }
+    }
+}
---
+++ DiceRoller_Solution/DiceRoller/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("> /roll 3d6 2d8");
+            Console.WriteLine();
+
+            Dice d6 = Dice.D6;
+
+            var total = 0;
+            for (var i = 0; i < 3; i++)
+            {
+                var roll = d6.Roll();
+                Console.WriteLine($"1{ d6.Description }: { roll }");
+                total += roll;
+            }
+
+            Dice d8 = Dice.D8;
+            for (var i = 0; i < 2; i++)
+            {
+                var roll = d8.Roll();
+                Console.WriteLine($"1{ d8.Description }: { roll }");
+                total += roll;
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Roll total: {total}");
+            Console.WriteLine();
+            Console.Write("> ");
+            Console.ReadLine();
+
+        }
+    }
+}
